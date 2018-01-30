ebd517ff4f280f61166742bbdd6ebf674e73765e
dmitriyprylutskyi <dprily@gmail.com>
2018-01-30 17:28:03 +0200

    4-1 block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Cars App';
+  title = 'Angular';
+  headerText = 'Create my directive.';
 }
