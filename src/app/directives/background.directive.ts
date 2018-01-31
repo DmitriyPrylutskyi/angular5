bbdc1867152046b2e4216b5b16ef48bc26b8ac97
dmitriyprylutskyi <dprily@gmail.com>
2018-01-31 16:40:17 +0200

    4-2 block

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,14 +1,16 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit{
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.el.nativeElement.style.backgroundColor = 'red';
+    const {nativeElement} = this.el;
+    this.renderer.setStyle(nativeElement, 'background-color', 'red');
+    this.renderer.addClass(nativeElement, 'white-text');
   }
 
 }
