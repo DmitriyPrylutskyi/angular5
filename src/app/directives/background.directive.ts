0737b0fa3b08b8d9a64e080f5fafafb63d0bba3a
dmitriyprylutskyi <dprily@gmail.com>
2018-01-31 17:24:36 +0200

    4-3 block

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,16 +1,28 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
-export class BackgroundDirective implements OnInit{
+export class BackgroundDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') background: string;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+
+  }
+
+  @HostListener('mouseenter') moiseEnter() {
     const {nativeElement} = this.el;
-    this.renderer.setStyle(nativeElement, 'background-color', 'red');
+    this.background = 'red';
     this.renderer.addClass(nativeElement, 'white-text');
+  }
+
+  @HostListener('mouseleave') moiseLeave() {
+    const {nativeElement} = this.el;
+    this.background = 'transparent';
+    this.renderer.removeClass(nativeElement, 'white-text');
   }
 
 }
