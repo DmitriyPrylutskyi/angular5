0fe494390d2405105d89208f0a5b6de0e967af4c
dmitriyprylutskyi <dprily@gmail.com>
2018-02-01 12:03:43 +0200

    5-1 block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
-  headerText = 'Create my directive.';
-  items = [1, 2, 3, 4 , 5];
-  current = 1;
-
-  onClick (number: number) {
-    this.current = number;
-  }
+  name = 'My App';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 }
