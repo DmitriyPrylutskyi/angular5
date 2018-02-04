c9f53e68813b2328d87de6be1cfdc856b4e342e8
unknown <dprily@gmail.com>
2018-02-04 14:50:40 +0200

    7-1 block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CarComponent } from './car/car.component';
-import { CarAddComponent } from './car-add/car-add.component';
-import { CarsService } from './cars.service';
-import { ConsoleService } from './console.service';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent,
-    CarAddComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [CarsService, ConsoleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
