2120ea40ef79b3f8510078d8b8c0018966dc644c
DmitriyPrylutskyi <dprily@gmail.com>
2018-02-06 14:15:00 +0200

    9-3 Navigation between pages

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomePageComponent },
