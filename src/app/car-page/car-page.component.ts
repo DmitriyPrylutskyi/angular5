26552529d03d48d2e0e5194725dbd2afdf2a5a22
DmitriyPrylutskyi <dprily@gmail.com>
2018-02-06 21:31:10 +0200

    9-6 Parameters of the address line. Part 2

--- src/app/car-page/car-page.component.ts
+++ src/app/car-page/car-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-page',
@@ -9,15 +9,41 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class CarPageComponent implements OnInit {
   id: number;
   name: string;
+  year: number;
+  color: string;
+  hash: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = +this.route.snapshot.params['id'];
+    this.name = this.route.snapshot.params['name'];
+    this.year = +this.route.snapshot.queryParams['year'];
+    this.color = this.route.snapshot.queryParams['color'];
+    this.hash = this.route.snapshot.fragment;
 
     this.route.params.subscribe((params: Params) => {
       this.name = params['name'];
+      this.id = params['id'];
+    });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.year = params['year'];
+      this.color = params['color'];
+    });
+
+    this.route.fragment.subscribe((fragment) => {
+      this.hash = fragment;
     });
   }
 
+  openLink() {
+    this.router.navigate(['/cars', 5, 'Mazda'], {
+      queryParams: {
+        color: 'yellow',
+        year: 2000
+      },
+      fragment: 'pics'
+    });
+  }
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,14 +5,20 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
+      year: 2017,
+      color: 'red',
       id: 1
     },
     {
       name: 'BMW',
+      year: 2016,
+      color: 'black',
       id: 2
     },
     {
       name: 'Audi',
+      year: 2015,
+      color: 'white',
       id: 3
     }
   ];
