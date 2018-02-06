ac1993a762be3d88597172f2ddd570fa2f81e47e
DmitriyPrylutskyi <dprily@gmail.com>
2018-02-06 21:53:35 +0200

    9-8 Redirect and error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { CarsService } from './cars.service';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     CarsPageComponent,
-    CarPageComponent
+    CarPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
